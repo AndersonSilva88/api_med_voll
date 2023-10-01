e8f20cb225d354f341b7622141f0298e54c13e66
AndersonOSilva <anderson.os88@hotmail.com>
2023-10-01 15:15:29 -0300

    update e delete

---
+++ src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
@@ -0,0 +1,15 @@
+package med.voll.api.medico;
+
+import jakarta.validation.constraints.NotBlank;
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosAtualizacaoMedico(
+        @NotBlank
+        Long id,
+        String nome,
+        String telefone,
+        DadosEndereco endereco
+
+    ) {
+
+}
